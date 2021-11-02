15cd24f028baef6c0c3899523700ecf2628d80a1
Mehul Vaghela <dev@ustechnologysolution.com>
2021-11-02 09:22:13 +0530

    First Commit

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,20 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Home msg="Welcome to Home page"/>
+    <router-view/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    Home
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Router from 'vue-router'
+import Home from './components/Home.vue'
+import Blog from './components/Blog.vue'
 
+Vue.use(Router);
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },{
+      path: '/blog',
+      name: 'blog',
+      component: Blog
+    }
+  ]
+})
